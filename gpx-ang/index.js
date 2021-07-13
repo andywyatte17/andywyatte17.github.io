81111dde75b3ca6558549df9e0923d9b76997086
Andrew Wyatt <andyw@emnetsoftware.com>
2021-07-13 08:08:37 +0100

    Updated gpx-ang website

--- gpx-ang/index.js
+++ gpx-ang/index.js
@@ -110,24 +110,24 @@ function save(position) {
         lat: at.latitude,
         long: at.longitude,
         timestamp: position.timestamp,
-        accuracy: off
+        accuracy: off,
     };
 
     /*
-            timeForNextUtterance = timeDelta + 30;
+              timeForNextUtterance = timeDelta + 30;
 
-            var s = RoundToDecimalPlaces(distanceTotal, 2).toString() + " km. ";
+              var s = RoundToDecimalPlaces(distanceTotal, 2).toString() + " km. ";
 
-            var km_per_second = distanceTotal / timeDelta;
-            var mins_per_km = 1.0 / km_per_second / 60.0;
-            mins_per_km = RoundToDecimalPlaces(mins_per_km, 1);
+              var km_per_second = distanceTotal / timeDelta;
+              var mins_per_km = 1.0 / km_per_second / 60.0;
+              mins_per_km = RoundToDecimalPlaces(mins_per_km, 1);
 
-            s += "Pace is " + mins_per_km + " minutes per km.";
+              s += "Pace is " + mins_per_km + " minutes per km.";
 
-            var synth = window.speechSynthesis;
-            var utterThis = new SpeechSynthesisUtterance(s);
-            synth.speak(utterThis);
-    */
+              var synth = window.speechSynthesis;
+              var utterThis = new SpeechSynthesisUtterance(s);
+              synth.speak(utterThis);
+      */
 
     coords.push(coordsThis);
 
@@ -136,16 +136,14 @@ function save(position) {
 
     var s = "";
     var bits = coords.slice(i0);
-    bits.forEach(
-        (bit) => {
-            s = s + JSON.stringify(bit) + "\n";
-        }
-    );
+    bits.forEach((bit) => {
+        s = s + JSON.stringify(bit) + "\n";
+    });
 
     var ds = distanceSum(coords);
     s = s + JSON.stringify(ds) + "\n";
     var kmPerMilli = ds.distanceKm / ds.timesum_millis;
-    var metresPerMinute = (kmPerMilli * 1000) * (60000);
+    var metresPerMinute = kmPerMilli * 1000 * 60000;
 
     s = s + metresPerMinute + " metres per minutes\n";
 
@@ -154,18 +152,17 @@ function save(position) {
 
 function distanceSum(someCoords) {
     var distance = 0;
-    var last, count = 0,
+    var last,
+        count = 0,
         timesum_millis = 0;
-    someCoords.forEach(
-        (bit, index) => {
-            if (index > 0) {
-                distance = distance + getDistance(last, bit);
-                timesum_millis += bit.timestamp - last.timestamp;
-                count += 1;
-            }
-            last = bit;
+    someCoords.forEach((bit, index) => {
+        if (index > 0) {
+            distance = distance + getDistance(last, bit);
+            timesum_millis += bit.timestamp - last.timestamp;
+            count += 1;
         }
-    );
+        last = bit;
+    });
     return { distanceKm: distance, count: count, timesum_millis: timesum_millis };
 }
 
@@ -201,11 +198,123 @@ function getDistance(coord1, coord2) {
 function copy() {
     var elem = document.getElementById("copy");
     var arr = Array();
-    coords.forEach(
-        function(value) {
-            arr.push([value.lat, value.long, value.timestamp]);
-        });
+    coords.forEach(function(value) {
+        arr.push([value.lat, value.long, value.timestamp]);
+    });
     elem.value = JSON.stringify(arr);
     elem.focus();
     elem.select();
+}
+
+function saveContent(fileContents, fileName) {
+    var link = document.createElement("a");
+    link.download = fileName;
+    link.href = "data:," + fileContents;
+    link.click();
+}
+
+function downloadGpsData() {
+    var Base64 = {
+        _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+        encode: function(e) {
+            var t = "";
+            var n, r, i, s, o, u, a;
+            var f = 0;
+            e = Base64._utf8_encode(e);
+            while (f < e.length) {
+                n = e.charCodeAt(f++);
+                r = e.charCodeAt(f++);
+                i = e.charCodeAt(f++);
+                s = n >> 2;
+                o = ((n & 3) << 4) | (r >> 4);
+                u = ((r & 15) << 2) | (i >> 6);
+                a = i & 63;
+                if (isNaN(r)) {
+                    u = a = 64;
+                } else if (isNaN(i)) {
+                    a = 64;
+                }
+                t =
+                    t +
+                    this._keyStr.charAt(s) +
+                    this._keyStr.charAt(o) +
+                    this._keyStr.charAt(u) +
+                    this._keyStr.charAt(a);
+            }
+            return t;
+        },
+        decode: function(e) {
+            var t = "";
+            var n, r, i;
+            var s, o, u, a;
+            var f = 0;
+            e = e.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+            while (f < e.length) {
+                s = this._keyStr.indexOf(e.charAt(f++));
+                o = this._keyStr.indexOf(e.charAt(f++));
+                u = this._keyStr.indexOf(e.charAt(f++));
+                a = this._keyStr.indexOf(e.charAt(f++));
+                n = (s << 2) | (o >> 4);
+                r = ((o & 15) << 4) | (u >> 2);
+                i = ((u & 3) << 6) | a;
+                t = t + String.fromCharCode(n);
+                if (u != 64) {
+                    t = t + String.fromCharCode(r);
+                }
+                if (a != 64) {
+                    t = t + String.fromCharCode(i);
+                }
+            }
+            t = Base64._utf8_decode(t);
+            return t;
+        },
+        _utf8_encode: function(e) {
+            e = e.replace(/\r\n/g, "\n");
+            var t = "";
+            for (var n = 0; n < e.length; n++) {
+                var r = e.charCodeAt(n);
+                if (r < 128) {
+                    t += String.fromCharCode(r);
+                } else if (r > 127 && r < 2048) {
+                    t += String.fromCharCode((r >> 6) | 192);
+                    t += String.fromCharCode((r & 63) | 128);
+                } else {
+                    t += String.fromCharCode((r >> 12) | 224);
+                    t += String.fromCharCode(((r >> 6) & 63) | 128);
+                    t += String.fromCharCode((r & 63) | 128);
+                }
+            }
+            return t;
+        },
+        _utf8_decode: function(e) {
+            var t = "";
+            var n = 0;
+            var r = (c1 = c2 = 0);
+            while (n < e.length) {
+                r = e.charCodeAt(n);
+                if (r < 128) {
+                    t += String.fromCharCode(r);
+                    n++;
+                } else if (r > 191 && r < 224) {
+                    c2 = e.charCodeAt(n + 1);
+                    t += String.fromCharCode(((r & 31) << 6) | (c2 & 63));
+                    n += 2;
+                } else {
+                    c2 = e.charCodeAt(n + 1);
+                    c3 = e.charCodeAt(n + 2);
+                    t += String.fromCharCode(
+                        ((r & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63)
+                    );
+                    n += 3;
+                }
+            }
+            return t;
+        },
+    };
+    var arr = Array();
+    coords.forEach(function(value) {
+        arr.push([value.lat, value.long, value.timestamp]);
+    });
+    var s = Base64.encode(JSON.stringify(arr));
+    saveContent("application/json;base64," + s, "track.gpx");
 }
