907a5e4bf4c25b682e900d2ee3ef47f98a804ca3
Andrew Wyatt <andyw@emnetsoftware.com>
2021-07-13 17:50:15 +0100

    Updated gpx-ang website

--- gpx-ang/index.js
+++ gpx-ang/index.js
@@ -42,9 +42,9 @@ window.onbeforeunload = function() {
 
 function init() {
     if (geoloc == mock_geoloc) {
-        document.getElementById("heading_log").text = "Log (mock)";
+        document.getElementById("heading_log").innerText = "Log (mock)";
     } else {
-        document.getElementById("heading_log").text = "Log (real)";
+        document.getElementById("heading_log").innerText = "Log (real)";
     }
 }
 
